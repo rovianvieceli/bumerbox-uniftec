3d514e10ffab51203cd24cd881ff25a6932cbc41
anubislopes <raphael.lopesrs@gmail.com>
2023-05-10 23:47:27 -0300

    adicionado funcionalidade de busca por cep

--- resources/views/fornecedor/edit.blade.php
+++ resources/views/fornecedor/edit.blade.php
@@ -48,3 +48,55 @@
         </div>
     </div>
 @endsection
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+<script>
+
+$(function(){
+    
+    cep.addEventListener('keyup', () => {
+        
+      var cep = $("#cep");
+      if (cep.val().length < 9) {
+        return;
+      }
+
+      $.ajax({
+
+        type:'POST',
+        url:"{{ route('busca-dados-cep') }}",
+        dataType: 'JSON',
+        data: {
+            "cep": cep.val(),
+        },
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        success:function(response){
+        
+            var dados = response.dados;
+            if (response.dados.erro) {
+                
+                alert("CEP invalido");
+                $("#cep").attr("value", "");
+                $("#endereco").attr("value", "");
+                return;
+            }
+            var endereco = dados.logradouro + 
+            ", Bairro: " + dados.bairro + 
+            ", Cidade: " + dados.cidade +
+            ", UF: " + dados.uf + 
+            ", CEP: " + dados.cep;
+            $("#endereco").attr("value", endereco);
+        }
+        });
+
+   });
+
+
+});
+
+</script>
+
